f97bb035d1477207502bbb5190053ed26a0730f7
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-19 23:25:07 +0530

    Delete Strapi security group from ECS configuration
    Removed security group configuration for Strapi.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -18,29 +18,6 @@ data "aws_subnets" "default" {
 }
 
 ############################
-# Security Group
-############################
-
-resource "aws_security_group" "strapi_sg" {
-  name_prefix = "strapi-sg-"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-############################
 # ECS Cluster
 ############################
 
