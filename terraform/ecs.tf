bafa7d7e28966335338e6e666b7eec4515438cf3
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-19 22:55:46 +0530

    Refactor ECS setup with new VPC and subnet resources

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,17 +2,52 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Get Default VPC
-data "aws_vpc" "default" {
-  default = true
+############################
+# VPC
+############################
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+############################
 # ECS Cluster
+############################
+
 resource "aws_ecs_cluster" "main" {
   name = "strapi-cluster"
 }
 
+############################
 # ECS Task Definition
+############################
+
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
@@ -39,7 +74,10 @@ resource "aws_ecs_task_definition" "strapi" {
   ])
 }
 
+############################
 # ECS Service
+############################
+
 resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.main.id
@@ -48,8 +86,10 @@ resource "aws_ecs_service" "strapi" {
   desired_count   = 1
 
   network_configuration {
-    subnets          = data.aws_subnets.default.ids
+    subnets          = [aws_subnet.public.id]
     assign_public_ip = true
-    security_groups  = [aws_security_group.strapi_sg_siva.id]
+    security_groups  = [aws_security_group.strapi_sg.id]
   }
+
+  depends_on = [aws_internet_gateway.igw]
 }
