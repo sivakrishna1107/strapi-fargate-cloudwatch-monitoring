cd2652b77d1ee63dc06761facaaec13a02d846e8
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-19 22:47:54 +0530

    Fix security group name formatting

--- terraform/security.tf
+++ terraform/security.tf
@@ -8,7 +8,7 @@ data "aws_subnets" "default" {
 
 # Security Group
 resource "aws_security_group" "strapi_sg_siva" {
-  name        = "strapi-sg_siva"
+  name        = "strapi-sg-siva"
   description = "Allow Strapi HTTP"
   vpc_id      = data.aws_vpc.default.id
 
