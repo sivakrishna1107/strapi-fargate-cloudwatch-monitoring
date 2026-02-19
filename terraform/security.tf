a704865bbab328d163624ecd2f8c38ca35e7854a
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-19 22:43:45 +0530

    Rename security group to 'strapi-sg_siva'

--- terraform/security.tf
+++ terraform/security.tf
@@ -8,7 +8,7 @@ data "aws_subnets" "default" {
 
 # Security Group
 resource "aws_security_group" "strapi_sg_siva" {
-  name        = "strapi-sg"
+  name        = "strapi-sg_siva"
   description = "Allow Strapi HTTP"
   vpc_id      = data.aws_vpc.default.id
 
