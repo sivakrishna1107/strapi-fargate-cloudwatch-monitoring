7966c650986615fa6b5d5f6806be84d8952c5df8
Sivakrishna117 <sivakrisnasasasa@gmail.com>
2026-02-19 17:29:05 +0530

    final fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "strapi-fargate-task"
+    bucket  = "strapi-fargate-task-siva"
     key     = "fargate/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
