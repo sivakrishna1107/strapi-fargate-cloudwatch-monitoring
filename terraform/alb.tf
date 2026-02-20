72f21268b4f3293e9d16166e2f9c13063843cce2
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-20 21:20:24 +0530

    Refactor ALB configuration and update listener action
    Removed default VPC and target group configuration. Updated ALB name and listener default action to fixed response.

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,44 +1,8 @@
-################################################
-# DEFAULT VPC
-################################################
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-################################################
-# APPLICATION LOAD BALANCER
-################################################
-
 resource "aws_lb" "alb" {
-  name               = "strapi-alb"
+  name               = "test-alb"
   load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = []
 }
-
-################################################
-# TARGET GROUP
-################################################
-
-resource "aws_lb_target_group" "tg" {
-  name        = "strapi-tg"
-  port        = 1337
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = data.aws_vpc.default.id
-}
-
-################################################
-# LISTENER
-################################################
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
@@ -46,7 +10,12 @@ resource "aws_lb_listener" "listener" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ok"
+      status_code  = "200"
+    }
   }
 }
