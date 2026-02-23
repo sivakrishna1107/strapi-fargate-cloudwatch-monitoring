295eed67f0274437d4a5e6ac87b9e154a45f1f20
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-23 16:10:45 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,3 @@
-variable "ecr_repo_url" {
-  description = "ECR repository URL for Strapi image"
-  type        = string
-}
-
-variable "database_url" {
-  description = "Strapi DATABASE_URL"
-  type        = string
-}
-
-variable "jwt_secret" {
-  description = "Strapi JWT_SECRET"
-  type        = string
-}
+variable "ecr_repo_url" { type = string }
+variable "database_url" { type = string }
+variable "jwt_secret" { type = string }
