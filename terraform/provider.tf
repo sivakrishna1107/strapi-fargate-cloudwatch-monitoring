ac6f9f908db0251372e4e0ce6bad60c9d040a537
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-23 14:38:40 +0530

    main is updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,29 +1,48 @@
-# Provider configuration
 provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
+  region = "us-east-1"  # Your region
 }
 
-# Data source for default VPC
+# Default VPC & Public Subnets (keep if ec2:DescribeVpcs works)
 data "aws_vpc" "default" {
   default = true
 }
 
-# Data source for default subnets (public subnets in default VPC)
-data "aws_subnets" "default_public" {
+data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
   filter {
     name   = "tag:Name"
-    values = ["*public*","*Public*"]
+    values = ["*public*", "*Public*"]
   }
 }
 
-# Data source for default ECS task execution role (ecsTaskExecutionRole)
-data "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRole"
+resource "random_id" "role_suffix" {
+  byte_length = 4
 }
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole-${random_id.role_suffix.hex}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Rest of your resources (cluster, task def, service, ALB) using:
+# execution_role_arn = aws_iam_role.ecs_task_execution.arn
+# subnets           = data.aws_subnets.public.ids
+
 
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
